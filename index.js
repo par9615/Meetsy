2beaa75bcd2fa9bee398848c0226f43d74ac9965
Jordan Terry <jterry@etsy.com>
2025-05-01 14:14:35 -0400

    feat: Initial set up work
    - `npm install --save-dev webpack`
    - `npm install @googleworkspace/meet-addons`

---
+++ index.js
@@ -0,0 +1,22 @@
+import { meet } from '@googleworkspace/meet-addons/meet.addons';
+
+const CLOUD_PROJECT_NUMBER = 'CLOUD_PROJECT_NUMBER';
+const MAIN_STAGE_URL = 'MAIN_STAGE_URL';
+
+/**
+ * Prepares the add-on Side Panel Client, and adds an event to launch the
+ * activity in the main stage when the main button is clicked.
+ */
+export async function setUpAddon() {
+    const session = await meet.addon.createAddonSession({
+        cloudProjectNumber: CLOUD_PROJECT_NUMBER,
+    });
+    const sidePanelClient = await session.createSidePanelClient();
+    document
+        .getElementById('start-activity')
+        .addEventListener('click', async () => {
+            await sidePanelClient.startActivity({
+                mainStageUrl: MAIN_STAGE_URL
+            });
+        });
+}
