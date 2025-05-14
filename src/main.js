49714a7e1b951701d7c3fcaa508245d3e89cb8b1
Miguel Hernandez <miguelhernandez@etsy.com>
2025-05-14 14:53:50 -0600

    fetch emails

--- src/main.js
+++ src/main.js
@@ -20,15 +20,10 @@ export async function setUpAddon() {
     });
 
     const sidePanelClient = await session.createSidePanelClient();
-    console.log("code: " + (await sidePanelClient.getMeetingInfo()).meetingCode)
-    console.log("id: " + (await sidePanelClient.getMeetingInfo()).meetingId)
-    // document
-    //     .getElementById('start-activity')
-    //     .addEventListener('click', async () => {
-    //         await sidePanelClient.startActivity({
-    //             mainStageUrl: MAIN_STAGE_URL
-    //         });
-    //     });
+    // console.log("code: " + (await sidePanelClient.getMeetingInfo()).meetingCode)
+    // console.log("id: " + (await sidePanelClient.getMeetingInfo()).meetingId)
+    const sessionId = (await sidePanelClient.getMeetingInfo()).meetingId;
+    return sessionId;
 }
 
 
@@ -37,36 +32,4 @@ export async function initializeMainStage() {
     cloudProjectNumber: CLOUD_PROJECT_NUMBER,
   });
   await session.createMainStageClient();
-}
-
-function oauthSignIn() {
-  console.log('OAUTHING');
-  // Google's OAuth 2.0 endpoint for requesting an access token
-  var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
-
-  // Create <form> element to submit parameters to OAuth 2.0 endpoint.
-  var form = document.createElement('form');
-  form.setAttribute('method', 'GET'); // Send as a GET request.
-  form.setAttribute('action', oauth2Endpoint);
-
-  // Parameters to pass to OAuth 2.0 endpoint.
-  var params = {'client_id': '597555850167-9vptt8pa3i7m5jheb59jrbu2m31ag9r7.apps.googleusercontent.com',
-                'redirect_uri': 'https://par9615.github.io/Meetsy/meetsy/SidePanel.html',
-                'response_type': 'token',
-                'scope': 'https://www.googleapis.com/auth/meetings.space.readonly',
-                'include_granted_scopes': 'true',
-                'state': 'pass-through value'};
-
-  // Add form parameters as hidden input values.
-  for (var p in params) {
-    var input = document.createElement('input');
-    input.setAttribute('type', 'hidden');
-    input.setAttribute('name', p);
-    input.setAttribute('value', params[p]);
-    form.appendChild(input);
-  }
-
-  // Add form to page and submit it to open the OAuth 2.0 endpoint.
-  document.body.appendChild(form);
-  form.submit();
 }
